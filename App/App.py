bde4a3eb18731861ec231f5549f24957f32aa87e
Narges Mousavi <nrgmsv@gmail.com>
2019-12-07 16:23:35 +0330

    bugfix

--- App/App.py
+++ App/App.py
@@ -7,15 +7,15 @@ class TopicModelingResource:
     def on_get(self, req, resp):
         """Handles GET requests"""
         resp.status = falcon.HTTP_200  # This is the default status
-        resp.body = ("thread id: "+threading.get_ident())
+        resp.body = ("thread id: "+str(threading.get_ident()))
 
 
 app = falcon.API()
 topicModelings = TopicModelingResource()
 app.add_route('/topicmodeling', topicModelings)
-# if __name__ == '__main__':
-#     with make_server('', 8000, app) as httpd:
+if __name__ == '__main__':
+    with make_server('', 8000, app) as httpd:
 
-#         print('Serving on port 8000...')
+        print('Serving on port 8000...')
 
-#         httpd.serve_forever()
+        httpd.serve_forever()
